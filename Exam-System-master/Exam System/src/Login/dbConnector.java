01f2ae0181e710980dcfd009d1e74c686b0790f0
RAUNAK ANSARI <you@example.com>
2023-06-01 18:39:17 +0530

    Commit

---
+++ Exam-System-master/Exam System/src/Login/dbConnector.java
@@ -0,0 +1,50 @@
+package Login;
+
+import java.sql.*;
+
+public class dbConnector {
+	static final String jdbc_driver = "com.mysql.jdbc.Driver";
+	// static final String db_url = "jdbc:mysql://localhost/examsystem";
+	static final String db_url = "jdbc:mysql://127.0.0.1:3306/examsystem";
+	static String user = "root", pass = "Maakapyaar@123";
+	// static String user="root",pass="mysql";
+	public static Connection conn = null;
+
+	public dbConnector() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver"); // Register JDBC driver
+			conn = DriverManager.getConnection(db_url, user, pass); // Connect to Database
+			// System.out.println("Database Selected ");
+		} catch (SQLException e) {
+			// Exceptions in JDBC
+			System.err.println("Exception  " + e.getMessage());
+		} catch (Exception e) {
+			// Exceptions in Class.forName()
+			System.err.println("Exception  " + e.getMessage());
+		} finally {
+			// Close all connections
+
+		}
+	}
+
+	public ResultSet executeQuery(String qry) {
+		ResultSet rs = null;
+		Statement stmt = null;
+		try {
+			stmt = this.conn.createStatement();
+			rs = stmt.executeQuery(qry);
+		} catch (Exception exp) {
+			System.err.println("In qry : " + qry);
+		} finally {
+			try {
+				stmt.close();
+			} catch (SQLException exp) {
+				// TODO Auto-generated catch block
+				exp.printStackTrace();
+			}
+		}
+		return rs;
+
+	}
+
+}
---
+++ Exam-System-master/Exam System/src/studentpanel/Demo.java
@@ -0,0 +1,13 @@
+package studentpanel;
+
+public class Demo {
+
+	/*
+	 * public static void main(String[] args) {
+	 * // TODO Auto-generated method stub
+	 * Exam ob=new Exam("EID");
+	 * //ob.setUndecorated(true);
+	 * }
+	 */
+
+}
